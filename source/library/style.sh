56f3ae7374f5da6099df42b9a8c079de31f58207
iamprogrammerlk <iamprogrammerlk@gmail.com>
2025-03-24 08:18:23 +0530

    Reorganize style functions

--- source/library/style.sh
+++ source/library/style.sh
@@ -1,296 +1,303 @@
 #!/bin/bash
 
-
 # BEWARE: There are some differences between colors when combining colors with bold text attribute:
-#         By default bold text has set foreground color to blue
+#         By default bold text has set foreground color to white
 
 # BEWARE: Below functions can't be use correctly with other string formatters 
 #         (like style_italic, style_underline, etc...), since it override text to bold by default.
-#         style_foreground_dark_gray
-#         style_foreground_light_red
-#         style_foreground_light_green
-#         style_foreground_yellow
-#         style_foreground_light_blue
-#         style_foreground_light_purple
-#         style_foreground_light_cyan
+# 
+#         style_foreground_blue_bold
+#         style_foreground_brown_bold
+#         style_foreground_cyan_bold
+#         style_foreground_gray_bold
+#         style_foreground_green_bold
+#         style_foreground_purple_bold
+#         style_foreground_red_bold
 #         style_foreground_white
-#         style_background_dark_gray
-#         style_background_light_red
-#         style_background_light_green
-#         style_background_yellow
-#         style_background_light_blue
-#         style_background_light_purple
-#         style_background_light_cyan
+# 
+#         style_background_blue_bold
+#         style_background_brown_bold
+#         style_background_cyan_bold
+#         style_background_gray_bold
+#         style_background_green_bold
+#         style_background_purple_bold
+#         style_background_red_bold
 #         style_background_white
 
+#  Text Formatting -----------------------------------------------------------------------------------------------------
 
 # Set string to normal characters
 # Parameter $1 string to be formatted (input)
 function style_normal()
 {
-  echo -e "\e[0m$1\e[0m"
+  echo -n -e "\033[0m$1\033[0m"
 }
 
 # Set string to bold characters
 # Parameter $1 string to be formatted (input)
 function style_bold()
 {
-  echo -e "\e[1m$1\e[0m"
+  echo -n -e "\033[0;1m$1\033[0m"
 }
 
-# TODO: \e[2m unknown character formatting
+# TODO: \033[0;2m unknown character formatting
 
 # Set string to italic characters
 # Parameter $1 string to be formatted (input)
 function style_italic()
 {
-  echo -e "\e[3m$1\e[0m"
+  echo -n -e "\033[0;3m$1\033[0m"
 }
 
 # Set string to underline characters
 # Parameter $1 string to be formatted (input)
 function style_underline()
 {
-  echo -e "\e[4m$1\e[0m"
+  echo -n -e "\033[0;4m$1\033[0m"
 }
 
 # Set string to blinking characters
 # Parameter $1 string to be formatted (input)
 function style_blinking()
 {
-  echo -e "\e[5m$1\e[0m"
+  echo -n -e "\033[0;5m$1\033[0m"
 }
 
-# TODO: \e[6m unknown character formatting
+# TODO: \033[0;6m unknown character formatting
 
 # Set string to reverse video characters
 # Parameter $1 string to be formatted (input)
 function style_reverse_video()
 {
-  echo -e "\e[7m$1\e[0m"
+  echo -n -e "\033[0;7m$1\033[0m"
 }
+
+
+#  Colors | Foreground -------------------------------------------------------------------------------------------------
 
 # Set string foreground color to black
 # Parameter $1 string to be formatted (input)
 function style_foreground_black()
 {
-  echo -e "\e[30m$1\e[0m"
-}
-
-# Set string foreground color to red
-# Parameter $1 string to be formatted (input)
-function style_foreground_red()
-{
-  echo -e "\e[31m$1\e[0m"
-}
-
-# Set string foreground color to green
-# Parameter $1 string to be formatted (input)
-function style_foreground_green()
-{
-  echo -e "\e[32m$1\e[0m"
-}
-
-# Set string foreground color to brown
-# Parameter $1 string to be formatted (input)
-function style_foreground_brown()
-{
-  echo -e "\e[33m$1\e[0m"
+  echo -n -e "\033[0;30m$1\033[0m"
 }
 
 # Set string foreground color to blue
 # Parameter $1 string to be formatted (input)
 function style_foreground_blue()
 {
-  echo -e "\e[34m$1\e[0m"
+  echo -n -e "\033[0;34m$1\033[0m"
 }
 
-# Set string foreground color to purple
+# Set string foreground color to blue with bold
 # Parameter $1 string to be formatted (input)
-function style_foreground_purple()
+function style_foreground_blue_bold()
 {
-  echo -e "\e[35m$1\e[0m"
+  echo -n -e "\033[1;34m$1\033[0m"
+}
+
+# Set string foreground color to brown
+# Parameter $1 string to be formatted (input)
+function style_foreground_brown()
+{
+  echo -n -e "\033[0;33m$1\033[0m"
+}
+
+# Set string foreground color to yellow
+# Parameter $1 string to be formatted (input)
+function style_foreground_brown_bold()
+{
+  echo -n -e "\033[1;33m$1\033[0m"
 }
 
 # Set string foreground color to cyan
 # Parameter $1 string to be formatted (input)
 function style_foreground_cyan()
 {
-  echo -e "\e[36m$1\e[0m"
+  echo -n -e "\033[0;36m$1\033[0m"
 }
 
-# Set string foreground color to light gray
+# Set string foreground color to cyan with bold
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_gray()
+function style_foreground_cyan_bold()
 {
-  echo -e "\e[37m$1\e[0m"
+  echo -n -e "\033[1;36m$1\033[0m"
 }
 
-# Set string foreground color to dark gray
+# Set string foreground color to gray
 # Parameter $1 string to be formatted (input)
-function style_foreground_dark_gray()
+function style_foreground_gray()
 {
-  echo -e "\e[1;30m$1\e[0m"
+  echo -n -e "\033[0;37m$1\033[0m"
 }
 
-# Set string foreground color to light red
+# Set string foreground color to gray with bold
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_red()
+function style_foreground_gray_bold()
 {
-  echo -e "\e[1;31m$1\e[0m"
+  echo -n -e "\033[1;30m$1\033[0m"
 }
 
-# Set string foreground color to light green
+# Set string foreground color to green
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_green()
+function style_foreground_green()
 {
-  echo -e "\e[1;32m$1\e[0m"
+  echo -n -e "\033[0;32m$1\033[0m"
 }
 
-# Set string foreground color to yellow
+# Set string foreground color to green with bold
 # Parameter $1 string to be formatted (input)
-function style_foreground_yellow()
+function style_foreground_green_bold()
 {
-  echo -e "\e[1;33m$1\e[0m"
+  echo -n -e "\033[1;32m$1\033[0m"
 }
 
-# Set string foreground color to light blue
+# Set string foreground color to purple
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_blue()
+function style_foreground_purple()
 {
-  echo -e "\e[1;34m$1\e[0m"
+  echo -n -e "\033[0;35m$1\033[0m"
 }
 
-# Set string foreground color to light purple
+# Set string foreground color to  purple with bold
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_purple()
+function style_foreground_purple_bold()
 {
-  echo -e "\e[1;35m$1\e[0m"
+  echo -n -e "\033[1;35m$1\033[0m"
 }
 
-# Set string foreground color to light cyan
+# Set string foreground color to red
 # Parameter $1 string to be formatted (input)
-function style_foreground_light_cyan()
+function style_foreground_red()
 {
-  echo -e "\e[1;36m$1\e[0m"
+  echo -n -e "\033[0;31m$1\033[0m"
+}
+
+# Set string foreground color to red with bold
+# Parameter $1 string to be formatted (input)
+function style_foreground_red_bold()
+{
+  echo -n -e "\033[1;31m$1\033[0m"
 }
 
 # Set string foreground color to white
 # Parameter $1 string to be formatted (input)
 function style_foreground_white()
 {
-  echo -e "\e[1;37m$1\e[0m"
+  echo -n -e "\033[1;37m$1\033[0m"
 }
+
+#  Colors | Background -------------------------------------------------------------------------------------------------
 
 # Set string background color to black
 # Parameter $1 string to be formatted (input)
 function style_background_black()
 {
-  echo -e "\e[40m$1\e[0m"
-}
-
-# Set string background color to red
-# Parameter $1 string to be formatted (input)
-function style_background_red()
-{
-  echo -e "\e[41m$1\e[0m"
-}
-
-# Set string background color to green
-# Parameter $1 string to be formatted (input)
-function style_background_green()
-{
-  echo -e "\e[42m$1\e[0m"
-}
-
-# Set string background color to brown
-# Parameter $1 string to be formatted (input)
-function style_background_brown()
-{
-  echo -e "\e[43m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;40m$1\033[0m"
 }
 
 # Set string background color to blue
 # Parameter $1 string to be formatted (input)
 function style_background_blue()
 {
-  echo -e "\e[44m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;44m$1\033[0m"
 }
 
-# Set string background color to purple
+# Set string background color to blue and bold
 # Parameter $1 string to be formatted (input)
-function style_background_purple()
+function style_background_blue_bold()
 {
-  echo -e "\e[45m$1\e[0m"
+  echo -n -e "\033[0;37m\033[1;44m$1\033[0m"
+}
+
+# Set string background color to brown
+# Parameter $1 string to be formatted (input)
+function style_background_brown()
+{
+  echo -n -e "\033[0;37m\033[0;43m$1\033[0m"
+}
+
+# Set string background color to brown and bold
+# Parameter $1 string to be formatted (input)
+function style_background_brown_bold()
+{
+  echo -n -e "\033[0;37m\033[1;43m$1\033[0m"
 }
 
 # Set string background color to cyan
 # Parameter $1 string to be formatted (input)
 function style_background_cyan()
 {
-  echo -e "\e[46m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;46m$1\033[0m"
 }
 
-# Set string background color to light gray
+# Set string background color to cyan  and bold
 # Parameter $1 string to be formatted (input)
-function style_background_light_gray()
+function style_background_cyan_bold()
 {
-  echo -e "\e[47m$1\e[0m"
+  echo -n -e "\033[0;37m\033[1;46m$1\033[0m"
 }
 
-
-# Set string background color to dark gray
+# Set string background color to gray
 # Parameter $1 string to be formatted (input)
-function style_background_dark_gray()
+function style_background_gray()
 {
-  echo -e "\e[1;40m$1\e[0m"
+  echo -n -e "\033[0;30m\033[0;47m$1\033[0m"
 }
 
-# Set string background color to light red
+# Set string background color to gray  and bold
 # Parameter $1 string to be formatted (input)
-function style_background_light_red()
+function style_background_gray_bold()
 {
-  echo -e "\e[1;41m$1\e[0m"
+  echo -n -e "\033[0;37m\033[1;40m$1\033[0m"
 }
 
-# Set string background color to light green
+# Set string background color to green
 # Parameter $1 string to be formatted (input)
-function style_background_light_green()
+function style_background_green()
 {
-  echo -e "\e[1;42m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;42m$1\033[0m"
 }
 
-# Set string background color to yellow
+# Set string background color to green  and bold
 # Parameter $1 string to be formatted (input)
-function style_background_yellow()
+function style_background_green_bold()
 {
-  echo -e "\e[1;43m$1\e[0m"
+  echo -n -e "\033[0;37m\033[1;42m$1\033[0m"
 }
 
-# Set string background color to light blue
+# Set string background color to purple
 # Parameter $1 string to be formatted (input)
-function style_background_light_blue()
+function style_background_purple()
 {
-  echo -e "\e[1;44m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;45m$1\033[0m"
 }
 
-# Set string background color to light purple
+# Set string background color to purple  and bold
 # Parameter $1 string to be formatted (input)
-function style_background_light_purple()
+function style_background_purple_bold()
 {
-  echo -e "\e[1;45m$1\e[0m"
+  echo -n -e "\033[0;37m\033[1;45m$1\033[0m"
 }
 
-# Set string background color to light cyan
+# Set string background color to red
 # Parameter $1 string to be formatted (input)
-function style_background_light_cyan()
+function style_background_red()
 {
-  echo -e "\e[1;46m$1\e[0m"
+  echo -n -e "\033[0;37m\033[0;41m$1\033[0m"
+}
+
+# Set string background color to red  and bold
+# Parameter $1 string to be formatted (input)
+function style_background_red_bold()
+{
+  echo -n -e "\033[0;37m\033[1;41m$1\033[0m"
 }
 
 # Set string background color to white
 # Parameter $1 string to be formatted (input)
 function style_background_white()
 {
-  echo -e "\e[1;47m$1\e[0m"
+  echo -n -e "\033[0;30m\033[1;47m$1\033[0m"
 }
+
--- source/playground.sh
+++ source/playground.sh
@@ -22,6 +22,13 @@ if [ ! -d "$APP_HOME" ]; then
   mkdir "$APP_HOME"
 fi
 
+if [ ! -f "$APP_ROOT/library/utility.sh" ]; then
+  echo "Runtime Error : '/library/utility.sh' is require to run '$APP_NAME'." >&2
+  echo ""
+  exit 1
+fi
+. $APP_ROOT/library/utility.sh
+
 if [ ! -f "$APP_ROOT/library/style.sh" ]; then
   echo "Runtime Error : '/library/style.sh' is require to run '$APP_NAME'." >&2
   echo ""
@@ -38,99 +45,123 @@ fi
 
 
 
+string_to_format="This string formatting is applied by calling function "
 
+# demo_style_text_format -----------------------------------------------------------------------------------------------
 
-# style_demo -----------------------------------------------------------------------------------------------------------
-function style_demo(){
-
-  string_to_format="This string formatting is applied by calling function "
-
+function demo_style_text_format(){
   style_normal "$string_to_format style_normal"
+  new_line
   style_bold "$string_to_format style_bold"
+  new_line
   style_italic "$string_to_format style_italic"
+  new_line
   style_underline "$string_to_format style_underline"
+  new_line
   style_blinking "$string_to_format style_blinking"
+  new_line
   style_reverse_video "$string_to_format style_reverse_video"
+  new_line
+}
 
-  echo ""
+# demo_style_text_foreground -------------------------------------------------------------------------------------------
 
+function demo_style_text_foreground(){
   style_foreground_black "$string_to_format style_foreground_black"
-  style_foreground_red "$string_to_format style_foreground_red"
-  style_foreground_green "$string_to_format style_foreground_green"
-  style_foreground_brown "$string_to_format style_foreground_brown"
+  new_line
   style_foreground_blue "$string_to_format style_foreground_blue"
-  style_foreground_purple "$string_to_format style_foreground_purple"
+  new_line
+  style_foreground_blue_bold "$string_to_format style_foreground_blue_bold"
+  new_line
+  style_foreground_brown "$string_to_format style_foreground_brown"
+  new_line
+  style_foreground_brown_bold "$string_to_format style_foreground_brown_bold"
+  new_line
   style_foreground_cyan "$string_to_format style_foreground_cyan"
-  style_foreground_light_gray "$string_to_format style_foreground_light_gray"
-
-  echo ""
-
-  style_foreground_dark_gray "$string_to_format style_foreground_dark_gray"
-  style_foreground_light_red "$string_to_format style_foreground_light_red"
-  style_foreground_light_green "$string_to_format style_foreground_light_green"
-  style_foreground_yellow "$string_to_format style_foreground_yellow"
-  style_foreground_light_blue "$string_to_format style_foreground_light_blue"
-  style_foreground_light_purple "$string_to_format style_foreground_light_purple"
-  style_foreground_light_cyan "$string_to_format style_foreground_light_cyan"
+  new_line
+  style_foreground_cyan_bold "$string_to_format style_foreground_cyan_bold"
+  new_line
+  style_foreground_gray "$string_to_format style_foreground_gray"
+  new_line
+  style_foreground_gray_bold "$string_to_format style_foreground_gray_bold"
+  new_line
+  style_foreground_green "$string_to_format style_foreground_green"
+  new_line
+  style_foreground_green_bold "$string_to_format style_foreground_green_bold"
+  new_line
+  style_foreground_purple "$string_to_format style_foreground_purple"
+  new_line
+  style_foreground_purple_bold "$string_to_format style_foreground_purple_bold"
+  new_line
+  style_foreground_red "$string_to_format style_foreground_red"
+  new_line
+  style_foreground_red_bold "$string_to_format style_foreground_red_bold"
+  new_line
   style_foreground_white "$string_to_format style_foreground_white"
+  new_line
+}
 
-  echo ""
+# demo_style_text_background -------------------------------------------------------------------------------------------
 
+function demo_style_text_background(){
   style_background_black "$string_to_format style_background_black"
-  style_background_red "$string_to_format style_background_red"
-  style_background_green "$string_to_format style_background_green"
-  style_background_brown "$string_to_format style_background_brown"
+  new_line
   style_background_blue "$string_to_format style_background_blue"
-  style_background_purple "$string_to_format style_background_purple"
+  new_line
+  style_background_blue_bold "$string_to_format style_background_blue_bold"
+  new_line
+  style_background_brown "$string_to_format style_background_brown"
+  new_line
+  style_background_brown_bold "$string_to_format style_background_brown_bold"
+  new_line
   style_background_cyan "$string_to_format style_background_cyan"
-  style_background_light_gray "$string_to_format style_background_light_gray"
-
-  echo ""
-
-  style_background_dark_gray "$string_to_format style_background_dark_gray"
-  style_background_light_red "$string_to_format style_background_light_red"
-  style_background_light_green "$string_to_format style_background_light_green"
-  style_background_yellow "$string_to_format style_background_yellow"
-  style_background_light_blue "$string_to_format style_background_light_blue"
-  style_background_light_purple "$string_to_format style_background_light_purple"
-  style_background_light_cyan "$string_to_format style_background_light_cyan"
+  new_line
+  style_background_cyan_bold "$string_to_format style_background_cyan_bold"
+  new_line
+  style_background_gray "$string_to_format style_background_gray"
+  new_line
+  style_background_gray_bold "$string_to_format style_background_gray_bold"
+  new_line
+  style_background_green "$string_to_format style_background_green"
+  new_line
+  style_background_green_bold "$string_to_format style_background_green_bold"
+  new_line
+  style_background_purple "$string_to_format style_background_purple"
+  new_line
+  style_background_purple_bold "$string_to_format style_background_purple_bold"
+  new_line
+  style_background_red "$string_to_format style_background_red"
+  new_line
+  style_background_red_bold "$string_to_format style_background_red_bold"
+  new_line
   style_background_white "$string_to_format style_background_white"
-
-  # combine use case
-
-  echo ""
-
-  echo "$(style_blinking "<<-- SAMPLE -->>") Mix the Foreground Color $(style_foreground_brown "Brown") with the $(style_foreground_yellow "Yellow") and Background Color $(style_background_purple " Purple ") on $(style_reverse_video "$(style_underline " BASH ")")"
-  echo ""
-
+  new_line
 }
-# style_demo
 
-# print_header ---------------------------------------------------------------------------------------------------------
-function print_header()
+# demo_style_combine_use_case ------------------------------------------------------------------------------------------
+
+function demo_style_combine_use_case()
 {
-  style_foreground_purple "$(ui_header_divider)"
-  style_foreground_purple "$(ui_header_fill)"
-  style_foreground_purple "$(ui_header_title_centered "$APP_NAME v$APP_VERSION")"
-  style_foreground_purple "$(ui_header_fill)"
-  style_foreground_purple "$(ui_header_divider)"
-  echo ""
+  echo -n "$(style_blinking "<<-- SAMPLE -->>") Mix the Foreground Color $(style_foreground_brown "Brown") with the " 
+  echo -n "$(style_foreground_brown_bold "Brown Bold") and Background Color $(style_background_purple " Purple ") on "
+  echo "$(style_reverse_video "$(style_underline " BASH ")")"
+  # No 'new_line' need here, becurse last 'echo' don't have '-n' flag
 }
-print_header
 
-# print_copyright ------------------------------------------------------------------------------------------------------
-function print_copyright()
-{
-  local msg_01="   Developer : $app_author [$app_author_url] "
-  local msg_02="     License : $app_license [$app_license_url]"
-  local msg_03="     Version : $APP_VERSION"
-  style_foreground_purple "$(ui_header_divider)"
-  style_foreground_purple "$(ui_header_fill)"
-  style_foreground_purple "$(ui_header_title_left "$msg_01")"
-  style_foreground_purple "$(ui_header_title_left "$msg_02")"
-  style_foreground_purple "$(ui_header_title_left "$msg_03")"
-  style_foreground_purple "$(ui_header_fill)"
-  style_foreground_purple "$(ui_header_divider)"
-  echo ""
-}
-print_copyright
+
+
+
+
+
+
+
+
+
+demo_style_text_format
+empty_line
+demo_style_text_foreground
+empty_line
+demo_style_text_background
+empty_line
+demo_style_combine_use_case
+empty_line
