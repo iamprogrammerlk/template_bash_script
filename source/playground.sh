fb553e00946e112b3a0a8d2769b2f5cf43cbce6b
iamprogrammerlk <iamprogrammerlk@gmail.com>
2025-08-04 10:26:07 +0530

    Add demo text styles

--- source/playground.sh
+++ source/playground.sh
@@ -39,3 +39,113 @@ if [ ! -f "$APP_ROOT/library/style.sh" ]; then
   exit 1
 fi
 . $APP_ROOT/library/style.sh
+
+
+
+# demo_style_text_format -----------------------------------------------------------------------------------------------
+demo_style_text_format(){
+  style_normal "$demo_string_to_be_format style_normal"
+  new_line
+  style_bold "$demo_string_to_be_format style_bold"
+  new_line
+  style_italic "$demo_string_to_be_format style_italic"
+  new_line
+  style_underline "$demo_string_to_be_format style_underline"
+  new_line
+  style_blinking "$demo_string_to_be_format style_blinking"
+  new_line
+  style_reverse_video "$demo_string_to_be_format style_reverse_video"
+  new_line
+}
+
+# demo_style_text_foreground -------------------------------------------------------------------------------------------
+demo_style_text_foreground(){
+  style_foreground_black "$demo_string_to_be_format style_foreground_black"
+  new_line
+  style_foreground_blue "$demo_string_to_be_format style_foreground_blue"
+  new_line
+  style_foreground_blue_bold "$demo_string_to_be_format style_foreground_blue_bold"
+  new_line
+  style_foreground_brown "$demo_string_to_be_format style_foreground_brown"
+  new_line
+  style_foreground_brown_bold "$demo_string_to_be_format style_foreground_brown_bold"
+  new_line
+  style_foreground_cyan "$demo_string_to_be_format style_foreground_cyan"
+  new_line
+  style_foreground_cyan_bold "$demo_string_to_be_format style_foreground_cyan_bold"
+  new_line
+  style_foreground_gray "$demo_string_to_be_format style_foreground_gray"
+  new_line
+  style_foreground_gray_bold "$demo_string_to_be_format style_foreground_gray_bold"
+  new_line
+  style_foreground_green "$demo_string_to_be_format style_foreground_green"
+  new_line
+  style_foreground_green_bold "$demo_string_to_be_format style_foreground_green_bold"
+  new_line
+  style_foreground_purple "$demo_string_to_be_format style_foreground_purple"
+  new_line
+  style_foreground_purple_bold "$demo_string_to_be_format style_foreground_purple_bold"
+  new_line
+  style_foreground_red "$demo_string_to_be_format style_foreground_red"
+  new_line
+  style_foreground_red_bold "$demo_string_to_be_format style_foreground_red_bold"
+  new_line
+  style_foreground_white "$demo_string_to_be_format style_foreground_white"
+  new_line
+}
+
+# demo_style_text_background -------------------------------------------------------------------------------------------
+demo_style_text_background(){
+  style_background_black "$demo_string_to_be_format style_background_black"
+  new_line
+  style_background_blue "$demo_string_to_be_format style_background_blue"
+  new_line
+  style_background_blue_bold "$demo_string_to_be_format style_background_blue_bold"
+  new_line
+  style_background_brown "$demo_string_to_be_format style_background_brown"
+  new_line
+  style_background_brown_bold "$demo_string_to_be_format style_background_brown_bold"
+  new_line
+  style_background_cyan "$demo_string_to_be_format style_background_cyan"
+  new_line
+  style_background_cyan_bold "$demo_string_to_be_format style_background_cyan_bold"
+  new_line
+  style_background_gray "$demo_string_to_be_format style_background_gray"
+  new_line
+  style_background_gray_bold "$demo_string_to_be_format style_background_gray_bold"
+  new_line
+  style_background_green "$demo_string_to_be_format style_background_green"
+  new_line
+  style_background_green_bold "$demo_string_to_be_format style_background_green_bold"
+  new_line
+  style_background_purple "$demo_string_to_be_format style_background_purple"
+  new_line
+  style_background_purple_bold "$demo_string_to_be_format style_background_purple_bold"
+  new_line
+  style_background_red "$demo_string_to_be_format style_background_red"
+  new_line
+  style_background_red_bold "$demo_string_to_be_format style_background_red_bold"
+  new_line
+  style_background_white "$demo_string_to_be_format style_background_white"
+  new_line
+}
+
+# demo_style_combine_use_case ------------------------------------------------------------------------------------------
+demo_style_combine_use_case()
+{
+  echo -n "$(style_blinking "<<-- SAMPLE -->>") Mix the Foreground Color $(style_foreground_brown "Brown") with the " 
+  echo -n "$(style_foreground_brown_bold "Brown Bold") and Background Color $(style_background_purple " Purple ") on "
+  echo "$(style_reverse_video "$(style_underline " BASH ")")"
+  # No 'new_line' need here, becurse last 'echo' don't have '-n' flag
+}
+
+
+
+demo_style_text_format
+empty_line
+demo_style_text_foreground
+empty_line
+demo_style_text_background
+empty_line
+demo_style_combine_use_case
+empty_line
